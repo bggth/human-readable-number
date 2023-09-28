edaa859a3b46aa6215216ce47a4fc1505f078a64
unknown <darkpers@gmail.com>
2023-09-28 14:33:48 +0300

    add: solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,60 @@
-module.exports = function toReadable (number) {
-  
+let zero = (number) => {
+    let english0 = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    return english0[number];
 }
+
+let ten = (number) => {
+    let english10 = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    return english10[number];
+}
+
+let tens = (number) => {
+    let english20 = ['', 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    return english20[number / 10];
+}
+
+let hundred = (number) => {
+    let english0 = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    return english0[number / 100] + ' hundred'
+}
+
+let toReadable_ = (number) => {
+    let result = ''
+    let functions = [zero, tens, hundred];
+    let numberString = number.toString();
+    let digits = numberString.split('').reverse();
+    let prev = -1;
+    try {
+        for (let i=0; i<digits.length; i++) {
+            let current = digits[i]*10**i;
+            if (current == 10) {
+                result = ten(prev);
+            } else {
+                result = functions[i](current) + ' ' + result;
+                prev = current;    
+            }
+        }
+    }
+    catch {
+        //console.log('error')
+    }
+    if (number == 0)
+        result = 'zero';
+    result = result.trim();
+    result = result.replace('  ', ' ')
+    //console.log(number, result)
+    return result;
+}
+
+let test = () => {
+    for (let i = 0; i <= 22; i++) {
+        console.log('--------');
+        toReadable_(i);
+    }
+}
+
+module.exports = function toReadable (number) {
+  return toReadable_(number);
+}
+
+test();
